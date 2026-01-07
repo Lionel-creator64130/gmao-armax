3de821764700028d8cde504fd09ca3b417264538
Lionel-creator64130 <lionnelfournier64370@gmail.com>
2026-01-07 15:52:18 +0100

    Update main.jsx

--- gmao_web_interne_modern/frontend/src/main.jsx
+++ gmao_web_interne_modern/frontend/src/main.jsx
@@ -1,9 +1,16 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const API_URL = "https://gmao-armax-1.onrender.com"
+
 function App() {
-  return <h1>GMAO Web Moderne</h1>
+  return (
+    <div>
+      <h1>GMAO Web Moderne</h1>
+      <p>Backend connecté :</p>
+      <p>{API_URL}</p>
+    </div>
+  )
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
